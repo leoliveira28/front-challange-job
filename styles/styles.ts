f88fab0c68cd7b96d51ae54757f695dff62f6d6e
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-03-27 00:17:50 -0300

    adjust index height

--- styles/styles.ts
+++ styles/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     widows: 1140px ;
-    height: 100vh;
+    
     display: flex;
     flex-direction: column;
     align-items: center;
